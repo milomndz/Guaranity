4798c2e195a3df35c6332d58ea6c543bd8913d2f
Héctor Alejandro Campos <h.alecampos96@gmail.com>
2019-11-26 09:28:16 -0600

    agregue Ruta y archivo sintactico
    Routing para analizador sintactico + declaraci[on de funcion para analizador sintactico

--- Guaranity/server/requestRouting.js
+++ Guaranity/server/requestRouting.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var Router = express.Router();
 var lexicografico = require("./lexicografico");
-
+var sintactico= require("./sintactico");
 Router.post("/lexi", function(req, res) {
   var query1 = req.body.var1;
   var code = lexicografico(query1);
@@ -13,10 +13,17 @@ Router.post("/all", function(req, res) {
   var codigo = req.bodyparser.codigo;
   res.send(codigo);
 });
+Router.post("/sintactico_semantico", function(req,res){
+var tokens=req.bodyparser.tokens;
+var code= req.body.codigo;
+var errores= sintactico(code, tokens);
+res.send(errores);
+});
 
 Router.all("*", function(req, res) {
   res.send("No se encontro el recurso solicitado");
   res.end();
 });
+
 
 module.exports = Router;
---
+++ Guaranity/server/sintactico.js
@@ -0,0 +1,20 @@
+var arreglos = require("./arreglos");
+module.exports = function sintactico(code, tokens){
+    var error = false;
+    
+    // Hacer logica del sintactico-semantico aqui
+    
+    if(error){
+        errores={
+            fallo: error,
+            mensajes: ""
+        }
+    }
+    else{
+        errores={
+            fallo: error,
+            mensajes= ""
+        }
+    }
+    return errores;
+}
